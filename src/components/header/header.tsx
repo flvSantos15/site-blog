7ffe0fb7d8b9ea722bff4df06c2d1e1b587fb1b9
flvSantos15 <flvsantos300@gmail.com>
2025-07-17 18:26:52 -0300

    docs: add active link component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,14 +1,8 @@
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useRouter } from "next/router";
+import { ActiveLink } from "../active-link";
 import { Button } from "../ui/button";
 
 export function Header() {
-  const router = useRouter();
-
-  const isHomePage = (router.pathname = "/");
-  const isBlogPage = router.pathname.startsWith("/blog");
-
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
@@ -16,17 +10,11 @@ export function Header() {
           <Link href="/"></Link>
 
           <nav className="flex items-center gap-6">
-            <Link
-              href="/"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-primary",
-                isHomePage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Inicio
-            </Link>
-            <Link href="/blog">Blog</Link>
-            <Button variant="secondary">Comecar</Button>
+            <ActiveLink href="/">Inicio</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Comecar</Link>
+            </Button>
           </nav>
         </div>
       </div>
